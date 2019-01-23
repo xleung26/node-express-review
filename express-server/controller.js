8a11c81031e037777f3108e8b8ba464599ec64ae
kyijulie <julieim@Julies-MacBook-Air.local>
2019-01-22 18:14:33 -0800

    set up routes and controller

---
+++ express-server/controller.js
@@ -0,0 +1,11 @@
+module.exports = {
+  fetch: (req, res) => {
+    console.log("IN GET");
+  },
+  post: (req, res) => {
+    console.log("IN POST");
+  },
+  delete: (req, res) => {
+    console.log("IN DELETE");
+  }
+};
--- express-server/index.js
+++ express-server/index.js
@@ -4,6 +4,7 @@ const parser = require("body-parser");
 Express: Parse incoming request bodies in a middleware before your handlers, available under the req.body property.
 */
 const path = require("path");
+const routes = require("./routes.js");
 const PORT = 3000;
 //middlewares: https://medium.com/@agoiabeladeyemi/a-simple-explanation-of-express-middleware-c68ea839f498
 const app = express();
@@ -15,6 +16,8 @@ app.use(parser.urlencoded({ extended: true }));
   from regular forms set to POST) and exposes the resulting object (containing the keys and values) 
   on req.body. */
 app.use(express.static(path.resolve(__dirname, "../static")));
+
+app.use("/api", routes);
 
 app.listen(PORT, () => {
   console.log("app is listening on PORT", PORT);
